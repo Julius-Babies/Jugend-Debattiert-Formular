a2b80f6cf181185dcbde23cd9b7a438474e6a3e9
Julius Vincent Babies <julius@familie-babies.de>
2025-03-09 20:33:25 +0100

    Initial commit

---
+++ src/lib/Scoring.svelte
@@ -0,0 +1,88 @@
+<script lang="ts">
+    let scoreSachkenntnis: number | undefined = $state(undefined);
+    let scoreAusdrucksvermoegen: number | undefined = $state(undefined);
+    let scoreGespraechsfaehigkeit: number | undefined = $state(undefined);
+    let scoreUeberzeugungskraft: number | undefined = $state(undefined);
+
+    let sum = $derived((scoreSachkenntnis ?? 0) + (scoreAusdrucksvermoegen ?? 0) + (scoreGespraechsfaehigkeit ?? 0) + (scoreUeberzeugungskraft ?? 0));
+</script>
+
+<table class="judge_points_table">
+    <tbody>
+    <tr>
+        <td>Sachkenntnis</td>
+        <td><select bind:value={scoreSachkenntnis}>
+            <option value={undefined} disabled selected>Nicht ausgewählt</option>
+            <option value={0}>0 - Kennt sich gar nicht aus, nur falsche Behauptungen</option>
+            <option value={1}>1 - Kennt sich kaum aus, vieles falsch</option>
+            <option value={2}>2 - Weiß einigermaßen Bescheid, manches teilweise richtig</option>
+            <option value={3}>3 - Kennt sich gut aus, kennt die wichtigsten Fakten</option>
+            <option value={4}>4 - Genau informiert, kennt alle wichtigen Gesichtspunkte</option>
+            <option value={5}>5 - Ausgezeichnet vorbereitet, hat alles parat, Detailwissen und Kenntnis der
+                Zusammenhänge
+            </option>
+        </select></td>
+    </tr>
+    <tr>
+        <td>Ausdrucksvermögen</td>
+        <td><select bind:value={scoreAusdrucksvermoegen}>
+            <option value={undefined} disabled selected>Nicht ausgewählt</option>
+            <option value={0}>0 - Völlig unverständlich</option>
+            <option value={1}>1 - Gerade noch verständlich</option>
+            <option value={2}>2 - Einigermaßen verständlich</option>
+            <option value={3}>3 - Verständlich und flüssig</option>
+            <option value={4}>4 - Klar, anschaulich und lebendig</option>
+            <option value={5}>5 - Jederzeit angemessen, einprägsam und originell</option>
+        </select></td>
+    </tr>
+    <tr>
+        <td>Gesprächsfähigkeit</td>
+        <td><select bind:value={scoreGespraechsfaehigkeit}>
+            <option value={undefined} disabled selected>Nicht ausgewählt</option>
+            <option value={0}>0 - Hört gar nicht zu, beteiligt sich gar nicht am Gespräch</option>
+            <option value={1}>1 - Hört wenig zu, redet kaum zu den Gesprächspartnern</option>
+            <option value={2}>2 - Hört meistens zu, geht manchmal auf die anderen ein</option>
+            <option value={3}>3 - Hört zum Teil gut zu, berücksichtigt die anderen meistens</option>
+            <option value={4}>4 - Hört gut zu, schließt gut an die anderen an</option>
+            <option value={5}>5 - Hört sehr aufmerksam zu, fragt nach, zeigt Übersicht, führt Gedanken der
+                anderen weiter
+            </option>
+        </select></td>
+    </tr>
+    <tr>
+        <td>Überzeugungskraft</td>
+        <td><select bind:value={scoreUeberzeugungskraft}>
+            <option value="{undefined}" disabled selected>Nicht ausgewählt</option>
+            <option value={0}>0 - Position völlig unklar, keine Begründungen</option>
+            <option value={1}>1 - Fast nur Behauptungen, Position kaum begründet</option>
+            <option value={2}>2 - Position teilweise begründet, viele Gründe eher schwach</option>
+            <option value={3}>3 - Teils schwache, teils gute Gründe</option>
+            <option value={4}>4 - Alles gut begründet, Gründe gut gewichtet</option>
+            <option value={5}>5 - In jeder Hinsicht überzeugend begründet, Blick für das Wesentliche im
+                Fortschritt der Debatte
+            </option>
+        </select></td>
+    </tr>
+    </tbody>
+</table>
+<span style="font-weight: bold; font-size: 24px">Summe: {sum}</span>
+
+<style>
+    .judge_points_table {
+        width: 100%;
+    }
+
+    .judge_points_table tr {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+
+    .judge_points_table tr td {
+        width: 50%;
+    }
+
+    .judge_points_table tr td select {
+        width: 100%;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,135 @@
+<script lang="ts">
+    import Scoring from "$lib/Scoring.svelte";
+    import {onMount} from "svelte";
+
+    let rows = $state(1);
+
+    onMount(() => {
+        window.onbeforeunload = function() {
+            return "Seite neuladen? Alle Eingaben werden verloren gehen!";
+        }
+    })
+</script>
+
+<svelte:head>
+    <title>Bewertungsbogen - Jugend Debattiert</title>
+</svelte:head>
+
+<h1 style="color: #f62a21">Bewertungsbogen Qualifikationsrunden</h1>
+<button onclick={() => window.print()} style="width: fit-content">Drucken</button>
+<div style="height: 16px"></div>
+<div style="display: flex; flex-direction: row; align-items: center; gap: 8px">
+    <span>Zur Frage:</span>
+    <input style="flex: 1; max-width: 600px" type="text" id="question"
+    placeholder="Sollten Schüler ihre Lehrer bewerten">
+    <span>?</span>
+</div>
+
+<div style="height: 16px"></div>
+
+<table style="width: 100%; border-collapse: collapse;" id="main_judge_table">
+    <thead>
+    <tr>
+        <th>
+            <div>Name Pro 1<input type="text" placeholder="Name Pro 1" id="t_pro1"></div>
+        </th>
+        <th>
+            <div>Name Contra 1<input type="text" placeholder="Name Contra 1" id="t_con1"></div>
+        </th>
+        <th>
+            <div>Name Pro 1<input type="text" placeholder="Name Pro 2" id="t_pro2"></div>
+        </th>
+        <th>
+            <div>Name Contra 2<input type="text" placeholder="Name Contra 2" id="t_con2"></div>
+        </th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>
+            <div><textarea placeholder="Eröffnungsrunde"></textarea></div>
+        </td>
+        <td>
+            <div><textarea placeholder="Eröffnungsrunde"></textarea></div>
+        </td>
+        <td>
+            <div><textarea placeholder="Eröffnungsrunde"></textarea></div>
+        </td>
+        <td>
+            <div><textarea placeholder="Eröffnungsrunde"></textarea></div>
+        </td>
+    </tr>
+    {#each {length: rows} as _, i}
+        <tr>
+            <td>
+                <div><textarea placeholder="Freie Aussprache"></textarea></div>
+            </td>
+            <td>
+                <div><textarea placeholder="Freie Aussprache"></textarea></div>
+            </td>
+            <td>
+                <div><textarea placeholder="Freie Aussprache"></textarea></div>
+            </td>
+            <td>
+                <div><textarea placeholder="Freie Aussprache"></textarea></div>
+            </td>
+        </tr>
+    {/each}
+    <tr id="add_row_row">
+        <td style="border: none !important" colspan="4">
+            <button onclick={() => rows += 1} style="width: 100%">+</button>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <div><textarea placeholder="Schlussrunde"></textarea></div>
+        </td>
+        <td>
+            <div><textarea placeholder="Schlussrunde"></textarea></div>
+        </td>
+        <td>
+            <div><textarea placeholder="Schlussrunde"></textarea></div>
+        </td>
+        <td>
+            <div><textarea placeholder="Schlussrunde"></textarea></div>
+        </td>
+    </tr>
+    <tr>
+        <td><Scoring/></td>
+        <td><Scoring/></td>
+        <td><Scoring/></td>
+        <td><Scoring/></td>
+    </tr>
+    </tbody>
+</table>
+
+<style>
+    th {
+        text-align: left;
+        font-weight: bold;
+        width: 25%;
+    }
+
+    th div, td div {
+        display: flex;
+        flex-direction: column;
+    }
+
+    th input, td textarea {
+        border: none;
+        min-height: 24px;
+    }
+
+    td {
+        border: 0.5px solid #000000;
+        flex: 1;
+    }
+
+    td textarea {
+        resize: vertical;
+        min-height: 64px;
+        height: auto;
+        overflow: auto;
+        background: transparent;
+    }
+</style>
